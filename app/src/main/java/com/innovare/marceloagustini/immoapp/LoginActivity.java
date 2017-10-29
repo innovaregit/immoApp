f70febb8c918a2b405b0151b9ab42f62ed690b9e
marceloagustini <agustinimarcelo@gmail.com>
2017-10-29 08:28:56 -0300

    Nuevo commit

---
+++ app/src/main/java/com/innovare/marceloagustini/immoapp/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.innovare.marceloagustini.immoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
